bfbf147166ae638caa1d19e8075161fafee9545c
alimotahhari <99986417+alimotahhari@users.noreply.github.com>
2022-02-20 04:59:15 +0330

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+import os
+os.system('cls')
+
+def calculate():
+    operation = input('''
+
++ 
+-
+* 
+/
+''')
+
+    number_1 = int(input(' first number: '))
+    number_2 = int(input(' second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+calculate()
