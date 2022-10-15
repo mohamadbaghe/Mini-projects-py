a441967a425bd1ba5ecc146f194d9fbfbd718e75
aliimthri <99986417+alimotahhari@users.noreply.github.com>
2022-10-15 13:14:45 -0700

    Update phone.py

--- phone.py
+++ phone.py
@@ -6,7 +6,8 @@ from phonenumbers import geocoder
 
 ch_number = phonenumbers.parse(number,'CH')
 print(geocoder.description_for_number(ch_number,"en"))
-
+#A
 from phonenumbers import carrier
 service_number = phonenumbers.parse(number,"RO")
 print(carrier.name_for_number(service_number,"en"))
+#.
